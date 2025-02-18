816bb2cffd91f108203871b683b292471b772fad
Matthew Lee <Matthew_Lee@avenues-ny.org>
2025-02-18 16:02:12 -0500

    PickNextMajor function
    have code analyze the raw csv and extract necessary information

--- Data_Visualization_2025_02_11_14_53_39/sketch.js
+++ Data_Visualization_2025_02_11_14_53_39/sketch.js
@@ -4,6 +4,10 @@ const majorsURL = "https://raw.githubusercontent.com/fivethirtyeight/data/refs/h
 
 let majorsTable
 let selectedMajors = []
+let selectedRates = []
+let currentIndex = 0
+let barWidth
+let maxHeight = 300 // tentative, can be changed as needed
 
 // ____________________________________________________________
 // Preload Function
@@ -19,6 +23,8 @@ function setup() {
   textSize(12)
   textAlign( CENTER,CENTER)
   noLoop()
+  pickNextMajors()
+  barWidth = width / (selectedMajors.length + 2) * 0.5
   
 }
 
@@ -26,10 +32,13 @@ function setup() {
 // Draw Function
 
 function draw() {
-  background(220);
+  background(220)
+  
+  let dx = width / (selectedMajors.length + 2)
   
   drawTitle()
   drawAxes()
+  YAxisLabel()
 }
 
 // ____________________________________________________________
@@ -50,9 +59,43 @@ function drawAxes() {
   stroke(0)
   
   // X-axis line
-  line(50, 350, width - 50, 350)
+  line(50, 350, width - 100, 350)
   
   // Y Axis Line
   line(50, 350, 50, 50)
 }
 // ____________________________________________________________
+// Y-axis Label
+
+function YAxisLabel() {
+  push()
+  textSize(14)
+  translate(30, height / 2 - 50)
+  rotate(-PI/2)
+  text("Employed Rate (%)", 0, 0)
+  pop()
+}
+
+// ____________________________________________________________
+// Function to Pick the Next 5 Majors (Goes in a sequence order)
+function pickNextMajors() {
+  selectedMajors = []
+  selectedRates = []
+
+  
+  for (let i = currentIndex; i < currentIndex + 5; i++) {
+    if (i < majorsTable.getRowCount()) {
+      let totalGrads = int(majorsTable.getString(i, "Total"))
+      let employedGrads = int(majorsTable.getString(i, "Employed"))
+
+      let employmentRate = (employedGrads / totalGrads) * 100
+      
+      selectedMajors.push(majorsTable.getString(i, "Major"))
+      selectedRates.push(employmentRate)
+    }
+  }
+
+  // Goes to the next five majors
+  // % is there so that if it goes over the number of majors, it loops back to the starting point
+  currentIndex = (currentIndex + 5) % majorsTable.getRowCount()
+}

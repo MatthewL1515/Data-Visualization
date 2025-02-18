5239545e4393dc55d0810c13e9e694bcd225b4f4
Matthew Lee <Matthew_Lee@avenues-ny.org>
2025-02-18 18:10:09 -0500

    Bars drawn in

--- Data_Visualization_2025_02_11_14_53_39/sketch.js
+++ Data_Visualization_2025_02_11_14_53_39/sketch.js
@@ -8,6 +8,7 @@ let selectedRates = []
 let currentIndex = 0
 let barWidth
 let maxHeight = 300 // tentative, can be changed as needed
+let dx
 
 // ____________________________________________________________
 // Preload Function
@@ -24,7 +25,6 @@ function setup() {
   textAlign( CENTER,CENTER)
   noLoop()
   pickNextMajors()
-  barWidth = width / (selectedMajors.length + 2) * 0.5
   
 }
 
@@ -34,11 +34,10 @@ function setup() {
 function draw() {
   background(220)
   
-  let dx = width / (selectedMajors.length + 2)
-  
   drawTitle()
   drawAxes()
   YAxisLabel()
+  drawBars(dx, barWidth)
 }
 
 // ____________________________________________________________
@@ -98,4 +97,37 @@ function pickNextMajors() {
   // Goes to the next five majors
   // % is there so that if it goes over the number of majors, it loops back to the starting point
   currentIndex = (currentIndex + 5) % majorsTable.getRowCount()
+}
+
+// ____________________________________________________________
+// Draw Bar Function
+
+function drawBars() {
+  dx = width / (selectedMajors.length + 2)
+  barWidth = dx * 0.5
+  let xAxisPos = 350
+  
+  for(let i = 0; i < selectedMajors.length; i++) {
+    let cx = dx * (i + 1)
+    let barHeight = map(selectedRates[i], 0, 100, 0, maxHeight)
+    
+    // In this function, I'm controlling the bar color so that a higher % is more blue while red is a lower %
+    let colorValue = map(selectedRates[i], 0, 100, 255, 0)
+    fill(colorValue, 150, 255 - colorValue)
+    
+    // Draw bar as rectangle
+    rect(cx - barWidth / 2, xAxisPos - barHeight, barWidth, barHeight)
+    
+    // Draw employment rate
+    fill(0)
+    textSize(10)
+    text(Math.round(selectedRates[i]) + "%", cx, xAxisPos - barHeight - 15)
+    
+    // Major name, wrapped function
+    fill(0)
+    textAlign(CENTER, TOP)
+    let maxWidth = barWidth - 10
+    textWrap(WORD)
+    text(selectedMajors[i], cx - 15, xAxisPos + 5, maxWidth)
+  }
 }
